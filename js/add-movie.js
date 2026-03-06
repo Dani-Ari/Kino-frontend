40c2087970a997e92b6f68aff0e503550d50caae
git-arian <195733915+git-arian@users.noreply.github.com>
2026-03-06 17:33:11 +0100

    feat(SCRUM-21): Create add movie form

---
+++ js/add-movie.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('addMovieForm');
+
+async function submitMovie(event) {
+    event.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const duration = document.getElementById('duration').value;
+    const ageLimit = document.getElementById('ageLimit').value;
+
+    const movie = {
+        title: title,
+        duration: duration,
+        ageLimit: ageLimit
+    };
+
+    const response = await fetch('http://localhost:8080/movies', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(movie)
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        alert('Movie added: ' + data.title)
+    } else {
+        alert('Something went wrong!')
+    }
+}
+
+form.addEventListener('submit', submitMovie);
